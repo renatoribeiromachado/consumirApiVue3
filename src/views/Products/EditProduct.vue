68fe8696901b644cd8956403b873dd71fad85fca
Renato Ribeiro Machado <63119764+renatoribeiromachado@users.noreply.github.com>
2023-03-16 13:36:59 -0300

    Update EditProduct.vue

--- src/views/Products/EditProduct.vue
+++ src/views/Products/EditProduct.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Editar Product</h1>
 
-        <form action="#" method="post" @submit.prevent="editProduct">
+        <form @submit.prevent="editProduct">
             <select name="category_id" v-model="product.category_id">
                 <option value="0">Selecione a categoria</option>
                 <option value="1"> Categoria A</option>
@@ -11,33 +11,33 @@
                 <option value="4"> Categoria D</option>
                 <option value="5"> Categoria E</option>
             </select>
+
             <input type="text" name="name" placeholder="Titulo do produto" v-model="product.name">
             <input type="text" name="description" placeholder="Descrição" v-model="product.description">
             <input type="file" name="image" ref="fileInput" placeholder="Imagem" @change="onFileChange">
-            <button type="submit"> Enviar
-            </button>
+            <button type="submit">Atualizar</button>
         </form>
     </div>
 </template>
 
 <script>
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, reactive } from 'vue'
 
 import ProductService from '@/services/products.service'
 import router from '@/router'
 
 export default {
+
     name: 'EditProduct',
+    
     props: {
         id: {
             require: true
         }
     },
+
     setup(props) {
 
-        const category_id = ref('')
-        const name = ref('')
-        const description = ref('')
         let imageUpload = null
 
         const product = reactive({
@@ -57,43 +57,46 @@ export default {
         onMounted(async () => {
             product.loading = true
             ProductService.getProduct(props.id)
-                        .then(response => {
-                            console.log(response);
-                            const productR = response.data
-                            product.category_id = productR.category_id
-                            product.name = productR.name
-                            product.description = productR.description
-                            product.completed = productR.completed == 'S'
-                        })
+            .then(response => {
+                console.log(response);
+                const productR = response.data
+                product.category_id = productR.category_id
+                product.name = productR.name
+                product.description = productR.description
+                product.completed = productR.completed == 'S'
+            })
               
         })
 
 
         const editProduct = () => {
+            
+            const formData = new FormData()
+            formData.append('category_id', "2")
+            formData.append('name', product.name)
+            formData.append('description', product.description)
+            formData.append('_method', 'PATCH')
+            
             if (imageUpload !== null){
-                
-                const formData = new FormData()
-                formData.append('image', imageUpload)
-                formData.append('category_id', category_id.value);
-                formData.append('name', name.value);
-                formData.append('description', description.value);
-            }  
-
+                formData.append('image', imageUpload);
+            }
             const config = {
                 headers:{
                     'content-type': 'multipart/form-data'
                 }
             }
 
-            ProductService.editProduct(props.id,config,formData, {...product})
+            ProductService.editProduct(props.id,formData,config)
                         .then(() => router.push({name: 'products.index'}))
   
         }
  
         return {
+          
             editProduct,
-            product,
-            onFileChange
+            onFileChange,
+            FormData,
+            product
         }
     }
 }
